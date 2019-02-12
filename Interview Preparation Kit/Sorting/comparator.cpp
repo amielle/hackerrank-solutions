de7e13480677a1e941011be6490d0ed43bfca966
amielle <43136926+amielle@users.noreply.github.com>
2019-02-12 19:24:19 +0800

    Create comparator.cpp

---
+++ Interview Preparation Kit/Sorting/comparator.cpp
@@ -0,0 +1,54 @@
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Player {
+    int score;
+    string name;
+};
+
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        // not sure why there is a need to use a class
+        // but since it's here why not try to use it
+        // I suppose it's helpful to make use of the customizable object
+        // when it comes to sorting in C++
+
+        if (a.score > b.score) return 1;
+        else if (b.score > a.score || b.name < a.name) return -1;
+        return 1; // for the case of a.name < b.name
+    }
+};
+
+
+
+
+bool compare(Player a, Player b) {
+    if(Checker::comparator(a,b) == -1)
+        return false;
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player player;
+        player.name = name, player.score = score;
+        players.push_back(player);
+    }
+    sort(players.begin(), players.end(), compare);
+    for(int i = 0; i < players.size(); i++) {
+        cout << players[i].name << " " << players[i].score << endl;
+    }
+    return 0;
+}
